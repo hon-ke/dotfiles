096d27c95e54678e4e1289d8c9d76ed363fb8951
clay <clay@hon-ker.cn>
2025-10-25 15:27:08 +0800

    add tide config to fish.sh

--- fish.sh
+++ fish.sh
@@ -4,3 +4,5 @@ curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fi
 fisher install jorgebucaran/fisher
 fisher install jethrokuan/z
 fisher install ilancosman/tide@v6
+
+tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time='24-hour format' --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
