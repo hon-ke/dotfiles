8f5d03e450d621ae1fef94e9df853d886ba9d470
clay <clay@hon-ker.cn>
2025-10-25 15:01:11 +0800

    chsh as fish

---
+++ fish.sh
@@ -0,0 +1,6 @@
+# 插件管理
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+fisher install jorgebucaran/fisher
+fisher install jethrokuan/z
+fisher install ilancosman/tide@v6
